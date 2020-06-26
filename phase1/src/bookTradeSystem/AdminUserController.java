fc3fce19717ed14b108f23b9a0a010c0abb6207f
Yu Xin Yan <eunice.yan@mail.utoronto.ca>
2020-06-26 17:03:00 -0400

    updated AdminUserController

--- phase1/src/bookTradeSystem/AdminUserController.java
+++ phase1/src/bookTradeSystem/AdminUserController.java
@@ -1,4 +1,34 @@
 package bookTradeSystem;
 
-public class AdminUserController {
+import java.io.Serializable;
+
+public class AdminUserController implements Serializable, Controllable {
+
+    UserManager um;
+    DisplaySystem ds;
+    FileWriter fw;
+    int adminUserId;
+
+    public AdminUserController(UserManager um, FileWriter fw, DisplaySystem ds, int adminUserId) {
+        this.um = um;
+        this.fw = fw;
+        this.ds = ds;
+        this.adminUserId = adminUserId;
+    }
+
+    @Override
+    public void alerts() {
+
+    }
+
+    @Override
+    public void actionResponse(int menuOption) {
+
+    }
+
+    @Override
+    public void serializeObjects() {
+
+    }
+
 }
