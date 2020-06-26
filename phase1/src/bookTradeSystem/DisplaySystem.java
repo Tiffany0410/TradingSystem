00a1a0f16dc7665af85e1bf9fca77daf7d4fa764
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-06-26 16:44:08 +0800

    Complete DisplaySystem, need to discuss with fileReader

--- phase1/src/bookTradeSystem/DisplaySystem.java
+++ phase1/src/bookTradeSystem/DisplaySystem.java
@@ -2,5 +2,29 @@ package bookTradeSystem;
 
 
 public class DisplaySystem {
+    FileReader menuOption;
+    FileReader notification;
+
+    /**
+     * Show the menu on the screen by taking the file name and show the options in the file
+     * to the screen
+     * In the feture, this will change to another UI not just text
+     * @param fileName
+     */
+
+    public void printMenu(String fileName){
+        System.out.println(menuOption.getMenu(fileName));
+    }
+
+    /**
+     * Show the notification for this user
+     * Get the username and looking the notification for this user in the file
+     * In the feture, this will change to another UI not just text
+     * @param userName
+     */
+
+    public void printNotification(String userName){
+        System.out.println(notification.getNotification(userName));
+    }
 
 }
