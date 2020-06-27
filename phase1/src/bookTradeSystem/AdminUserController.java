2e9b0b695761fdf3acfaca440e7893d7c9ca5291
Yu Xin Yan <eunice.yan@mail.utoronto.ca>
2020-06-27 12:23:05 -0400

    Updated structure for AdminUserController and RegularUserController

--- phase1/src/bookTradeSystem/AdminUserController.java
+++ phase1/src/bookTradeSystem/AdminUserController.java
@@ -4,30 +4,93 @@ import java.io.Serializable;
 
 public class AdminUserController implements Serializable, Controllable {
 
-    UserManager um;
+    /**UserManager um;
     DisplaySystem ds;
-    FileWriter fw;
+    FileWriter fw;**/
     int adminUserId;
 
-    public AdminUserController(UserManager um, FileWriter fw, DisplaySystem ds, int adminUserId) {
-        this.um = um;
-        this.fw = fw;
-        this.ds = ds;
+    public AdminUserController(int adminUserId) {
         this.adminUserId = adminUserId;
     }
 
     @Override
     public void alerts() {
-
+//      TODO: TO BE DECIDED
     }
 
     @Override
-    public void actionResponse(int menuOption) {
+    public void actionResponse(int mainMenuOption, int subMenuOption) {
+         /*
+        1. decide the menu options
+        2. decide what use case method to call for each menu option (discuss with Gabriel)
+        3. decide what presenter method to call to print the results for each menu option (discuss with Jiaqi)
+
+        */
+        switch(mainMenuOption){
+            case 1:
+                adminManageUsersMenuResponse(subMenuOption);
+                break;
+            case 2:
+                adminEditThresholdMenuResponse(subMenuOption);
+                break;
+            case 3:
+                adminOthersMenuResponse(subMenuOption);
+                break;
+
+        }
 
     }
 
-    @Override
-    public void serializeObjects() {
+    private void adminManageUsersMenuResponse(int subMenuOption) {
+        /*1.Freeze/ unfreeze users (system auto-freeze and admin can freeze and unfreeze)
+        2.Confirm and add item to user’s inventory
+        3.Remove item from user’s wishlist
+         */
+
+        switch (subMenuOption) {
+            case 1:
+//                TODO
+                break;
+            case 2:
+//                TODO
+                break;
+            case 3:
+//                TODO
+                break;
+
+        }
+    }
+
+    private void adminEditThresholdMenuResponse(int subMenuOption) {
+        /*
+        1.Edit the max number of transactions allowed a week
+        2.Edit the max number of transactions that can be incomplete before the account is frozen
+        3.Edit the number of books users must lend before users can borrow
+        4.Edit the max Edits per user for meeting’s date + time
+         */
+        switch (subMenuOption) {
+            case 1:
+//                TODO
+                break;
+            case 2:
+//                TODO
+                break;
+            case 3:
+//                TODO
+                break;
+            case 4:
+//                TODO
+                break;
+        }
+    }
+
+    private void adminOthersMenuResponse(int subMenuOption) {
+        /*
+        Add subsequent admin users
+         */
+        if (subMenuOption == 1){
+//            TODO: add other adminUser
+        }
 
     }
 
