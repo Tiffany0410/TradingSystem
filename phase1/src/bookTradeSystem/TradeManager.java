e9bf3288b4a9098c0abdc0e63705a75505031ebe
baoyuanz <yuanze.bao>
2020-06-26 21:18:23 -0400

    version1

--- phase1/src/bookTradeSystem/TradeManager.java
+++ phase1/src/bookTradeSystem/TradeManager.java
@@ -1,4 +1,128 @@
 package bookTradeSystem;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.Serializable;
+public class TradeManager implements Serializable {
+    private List<Trade> listTrade;
 
-public class TradeManager {
+    /** Constructor with a list of trade
+     * @param listTrade list of Trade
+     */
+    public TradeManager(List<Trade> listTrade){
+        this.listTrade = listTrade;
+    }
+
+    /**
+     * @return the list of trade
+     */
+    public List<Trade> getListTrade(){
+        return listTrade;
+    }
+
+    /** Set the old listTrade to new listTrade
+     * @param listTrade a list of trade
+     */
+    public void setListTrade(List<Trade> listTrade){
+        this.listTrade = listTrade;
+    }
+
+    /** Check a user history
+     * @param userId the user's id we want to check
+     * @return a list of that user's trade history
+     */
+    public List<Trade> getTradeHistory(int userId){
+        List<Trade> list = new ArrayList<>();
+        for (Trade t: listTrade){
+            if(t.getIds().get(1) == userId || t.getIds().get(2) == userId){
+                list.add(t);
+            }
+        }
+        return list;
+    }
+
+    /** Get a list of  user's Trades which its status is open
+     * @param userId the user's id we want to check
+     * @return the list of  user's Trades which its status is open
+     */
+    public List<Trade> getOpenTrade(int userId){
+        List<Trade> list = this.getTradeHistory(userId);
+        List<Trade> list1 = new ArrayList<>();
+        for(Trade t: list){
+            if (t.tradeStatus.equals("Open")){
+                list1.add(t);
+            }
+        }
+        return list1;
+    }
+
+    /** Get list of  user's Trades which its status is Closed
+     * @param userId the user's id we want to check
+     * @return the list of  user's Trades which its status is closed
+     */
+    public List<Trade> getClosedTrade(int userId){
+        List<Trade> list = this.getTradeHistory(userId);
+        List<Trade> list1 = new ArrayList<>();
+        for(Trade t: list){
+            if (t.tradeStatus.equals("Closed")){
+                list1.add(t);
+            }
+        }
+        return list1;
+    }
+
+    /** Get list of trade which its status is wait to be opened
+     * @param userId the user's id we want to check
+     * @return the list of trade which its status is wait to be opened
+     */
+    public List<Trade> getWaitTrade(int userId){
+        List<Trade> list = this.getTradeHistory(userId);
+        List<Trade> list1 = new ArrayList<>();
+        for(Trade t: list){
+            if (t.tradeStatus.equals("Wait to be opened")){
+                list1.add(t);
+            }
+        }
+        return list1;
+    }
+
+    /** Create a trade
+     * @param borrowerId borrower id
+     * @param lenderId lender id
+     * @param itemId item id
+     * @param tradeType type of the trade
+     * @return a Trade
+     */
+    public Trade createTrade(int borrowerId, int lenderId, int itemId, String tradeType){
+        return new Trade(borrowerId, lenderId, itemId, tradeType);
+    }
+
+    /** remove the trade from the list of trade
+     * @param tradeId trade id we want to remove
+     * @return the trade we remove
+     */
+    public Trade removeTrade(int tradeId) {
+        for (Trade t : listTrade) {
+            if (t.getIds().get(0) == tradeId) {
+                listTrade.remove(t);
+                return t;
+            }
+        }
+        return null;
+    }
+
+
+    /** Check if the trade is confirmed
+     * @param tradeId the id of the trade
+     * @return true if is, otherwise false
+     */
+    public boolean confirmComplete(int tradeId){
+        for (Trade t : listTrade) {
+            if (t.getIds().get(0) == tradeId){
+                if(t.tradeStatus.equals("Closed")){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
