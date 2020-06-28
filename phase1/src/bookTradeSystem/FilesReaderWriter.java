a591cb2e5895816ba76cad2322231af417dc62d0
chengleyang <chle.yang@mail.utoronto.ca>
2020-06-28 01:32:57 -0400

    filesreaderwriter draft2.0

--- phase1/src/bookTradeSystem/FilesReaderWriter.java
+++ phase1/src/bookTradeSystem/FilesReaderWriter.java
@@ -1,28 +1,9 @@
 package bookTradeSystem;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Logger;
-
-import bookTradeSystem.*;
 
 /**
  * Manages the saving and loading of objects.
@@ -164,3 +145,4 @@ public class FilesReaderWriter implements Serializable {
         }
     }
 }
+
