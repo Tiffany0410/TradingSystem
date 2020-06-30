1a814a6733647e7f4b4080689bc96a0271a8f2d6
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-06-30 16:40:36 +0800

    Complete frame of DemoMain

--- phase1/src/bookTradeSystem/DemoMain.java
+++ phase1/src/bookTradeSystem/DemoMain.java
@@ -1,4 +1,28 @@
 package bookTradeSystem;
 
+
+import java.io.IOException;
+import java.util.Map;
+
 public class DemoMain {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        DemoMainManager();
+    }
+
+    public static void DemoMainManager() throws IOException, ClassNotFoundException {
+        UserManager userManager = new UserManager();
+        MeetingManager meetingManager = new MeetingManager();
+        FilesReaderWriter filesReaderWriter = new FilesReaderWriter();
+        TradeManager tradeManager = new TradeManager();
+        DisplaySystem displaySystem = new DisplaySystem();
+        AccountCreator accountCreator = new AccountCreator(userManager, displaySystem, filesReaderWriter);
+
+        Map<String, String> userLoginInfo = filesReaderWriter.readUserInfo("./src/bookTradeSystem/RegularUserAccounts.csv");
+        Map<String, String> adminUserLoginInfo = filesReaderWriter.readUserInfo("./src/bookTradeSystem/AdminUserAccounts.csv");
+        LoginValidator loginValidator = new LoginValidator(userLoginInfo, adminUserLoginInfo);
+
+        TradingSystem tradingSystem = new TradingSystem(userManager, meetingManager, loginValidator, tradeManager,
+                filesReaderWriter, displaySystem, accountCreator);
+    }
+
 }
