fc12717f59c79bdf5d936d239dd7d7231ad7c555
Yu Xin Yan <eunice.yan@mail.utoronto.ca>
2020-07-08 16:58:57 -0400

    made changes to resolve some bugs For trade - I set all initial status to disagree (one way and two way trade)

--- phase1/src/bookTradeSystem/RegularUserDateTimeGetter.java
+++ phase1/src/bookTradeSystem/RegularUserDateTimeGetter.java
@@ -31,40 +31,50 @@ public class RegularUserDateTimeGetter {
 
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH-mm");
         List<Integer> list = new ArrayList<>();
-        boolean condition = true;
+        boolean ok = true;
+        int year = 0, month = 0, day = 0, hour = 0, minute = 0;
+        Scanner sc = new Scanner(System.in);
 
-        while (condition) {
+        do {
+            ok = true;
             ds.printOut("Please enter the date between 2020 to 2030 (Format: yyyy-MM-dd HH-mm)");
 
-            Scanner sc = new Scanner(System.in);
-            String typeIn = sc.nextLine();
-
             try {
+                String typeIn = sc.nextLine();
+
                 Date date = format.parse(typeIn);
 
-            } catch (ParseException e) {
-                ds.printOut("Wrong date format, please enter the date follow the format.");
-            }
+                year = Integer.parseInt(typeIn.substring(0, 4));
+                month = Integer.parseInt(typeIn.substring(5, 7));
+                day = Integer.parseInt(typeIn.substring(8, 10));
+                hour = Integer.parseInt(typeIn.substring(11, 13));
+                minute = Integer.parseInt(typeIn.substring(14, 16));
 
-            int year = Integer.parseInt(typeIn.substring(0, 4));
-            int month = Integer.parseInt(typeIn.substring(5, 7));
-            int day = Integer.parseInt(typeIn.substring(8, 10));
-            int hour = Integer.parseInt(typeIn.substring(11, 13));
-            int minute = Integer.parseInt(typeIn.substring(14, 16));
+            } catch (ParseException e) {
+                ds.printOut("Wrong date format, please follow the correct format. (Format: yyyy-MM-dd HH-mm)(ex. 2025-09-01 08-30)");
+                ok = false;
+
+            } catch (NumberFormatException e) {
+                ds.printOut("Please follow the correct date format! (Format: yyyy-MM-dd HH-mm)(ex. 2025-09-01 08-30)");
+                ok = false;
+
+            } catch (StringIndexOutOfBoundsException s) {
+                ds.printOut("Follow the correct date format! (Format: yyyy-MM-dd HH-mm)(ex. 2025-09-01 08-30)");
+                ok = false;
+            }
 
             if (isValidDay(year, month, day) && isValidTime(hour, minute)) {
                 ds.printOut("Time set success");
             } else {
-                ds.printOut("Please enter the year between 2020 to 2030");
+                ds.printOut("Time set failed. Try again");
+                ok = false;
             }
-            condition = false;
             list.add(year);
             list.add(month);
             list.add(day);
             list.add(hour);
             list.add(minute);
-        }
-
+        } while(!ok);
         return list;
 
     }
@@ -72,7 +82,7 @@ public class RegularUserDateTimeGetter {
     private boolean isValidTime(int hour, int min){return 1 <= hour && hour <= 24 && 0 <= min && min <= 59;}
 
     private boolean isValidDay(int year, int month, int day){
-        if (year <= 2020 || year >= 2030){return false;}
+        if (year < 2020 || year > 2030){return false;}
 
         if (month < 1 || month > 12){return false;}
 
