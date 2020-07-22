997783c263ac4482a000bfd13aaa002a3890f9f2
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-07-22 10:44:10 +0800

    Update for guest Login

--- phase2/src/controllers/LoginValidator.java
+++ phase2/src/controllers/LoginValidator.java
@@ -7,12 +7,14 @@ import java.util.Map;
  * A login validator of this system that stores username and password for users and administrative users.
  *
  *
- * @author Shi Tang
+ * @author Shi Tang, Jiaqi Gong
  * @version IntelliJ IDEA 2020.1.1
  */
 public class LoginValidator implements Serializable {
     private Map<String, String> userLoginInfo;
     private Map<String, String> adminUserLoginInfo;
+    private String userType;
+    private String userName;
 
     /** Constructor of LoginValidator
      * Set this userLoginInfo with userLoginInfo, set this adminUserLoginInfo with adminUserLoginInfo
@@ -24,6 +26,47 @@ public class LoginValidator implements Serializable {
         this.userLoginInfo = userLoginInfo;
         this.adminUserLoginInfo = adminUserLoginInfo;
     }
+
+    /**
+     * Check whether this username exists
+     * @param username The user name uer put in
+     * @return true if this user name exists
+     */
+
+    public boolean checkUsername(String  username){
+        if (userLoginInfo.containsKey(username)){
+            this.userType = "User";
+            this.userName = username;
+            return true;
+        } else if (adminUserLoginInfo.containsKey(username)){
+            this.userType = "Admin";
+            this.userName = username;
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    /**
+     *  Check the password for this user put in
+     * @param password password user put in
+     * @return string "User" if and only if user is user with correct username and password; return string "Admin" if
+     * and only if user is administrative user with correct username and password; otherwise, return "False".
+     */
+
+    public String checkPassword(String password){
+        if (this.userType.equals("User")){
+            if (userLoginInfo.get(this.userName).equals(password)) {
+                return "User";
+            }
+        } else if (this.userType.equals("Admin")){
+            if (adminUserLoginInfo.get(this.userName).equals(password)){
+                return "Admin";
+            }
+        }
+        return "False";
+    }
+
 
     /** Verify user's login with username and password
      * @param username The user's username
