3bf4ac79a18763dd0cdc6e0101a522e775aed96b
Jian Hong <jianhong.guo@mail.utoronto.ca>
2020-07-21 21:05:43 -0400

    update ReviewReportManager

--- phase2/src/Managers/FeedbackManager/ReviewReportManager.java
+++ phase2/src/Managers/FeedbackManager/ReviewReportManager.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ReviewReportManager {
-    // the key - id of user report others, the value is the list of users who is reported by the user.
+    // the key - id of user who has been reported, the value is the list of users who report the user.
     private Map<Integer, ArrayList<Integer>> reportMap;
     // the key is the id of the user who is reviewed, the value is the list of point the user get.
     private Map<Integer, ArrayList<Integer>> reviewMap;
@@ -30,4 +30,43 @@ public class ReviewReportManager {
             reviewMap.put(userId1,listPoint);
         }
     }
-}
+    public double calculateRate(int userId){
+        if (!reviewMap.containsKey(userId)){
+            return -1.0;
+        }else{
+            ArrayList<Integer> listPoint = reviewMap.get(userId);
+            Integer sum = 0;
+            for(Integer point: listPoint){
+                sum += point;
+            }
+            return sum.doubleValue() / listPoint.size(); }
+    }
+    // userId1 is the id of the user who has been reported, the userId2 is the id of user who report the other user
+    public boolean updateReport(int userId1, int userId2){
+        if (reportMap.containsKey(userId1) && reportMap.get(userId1).contains(userId2)){
+            return false;
+        }else if(reportMap.containsKey(userId1) && !reportMap.get(userId1).contains(userId2)){
+            reportMap.get(userId1).add(userId2);
+            return true;
+        }else {
+            ArrayList<Integer> listReport = new ArrayList<>();
+            listReport.add(userId2);
+            reportMap.put(userId1, listReport);
+            return true;
+        }
+        }
+    // userId1 is the id of the user who has been reported, the userId2 is the id of user who report the other user
+     public boolean deleteReport(int userId1, int userId2){
+         if (reportMap.containsKey(userId1) && reportMap.get(userId1).contains(userId2)){
+             if (reportMap.get(userId1).size() == 1){
+                 reportMap.remove(userId1);
+             }
+             else{
+                 reportMap.get(userId1).remove(userId2);
+             }
+             return true;
+         }return false;
+     }
+    }
+
+
