af071ba0d52c36f41ce74b6de19fdcc9a314a938
chengleyang <chle.yang@mail.utoronto.ca>
2020-07-24 23:42:50 -0400

    1)refactor the ser and csv files 2)create action entity and actionManager use case for mandatory extension

---
+++ phase2/src/managers/actionmanager/ActionManager.java
@@ -0,0 +1,13 @@
+package managers.actionmanager;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Store all the actions done by regularUser and adminUser
+ * @author Chengle Yang
+ * @version IntelliJ IDEA 2020.1
+ */
+public class ActionManager {
+
+}
