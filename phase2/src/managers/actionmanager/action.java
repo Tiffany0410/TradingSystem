905a98c6f1ffe81eb6c7bec460ae997b8719aac7
chengleyang <chle.yang@mail.utoronto.ca>
2020-07-25 00:33:43 -0400

    update action and actionmanager

--- phase2/src/managers/actionmanager/action.java
+++ phase2/src/managers/actionmanager/action.java
@@ -1,9 +1,14 @@
 package managers.actionmanager;
 
+import java.io.Serializable;
+
 /**
- * Store the key info of the action which done by regularUser and adminUser
+ * Store the key info of the action which can be cancelled
  * @author Chengle Yang
  * @version IntelliJ IDEA 2020.1
  */
-public class action {
+public class Action implements Serializable {
+    private int actionID;
+    private int actionOwnerID;
+    private int itemID;
 }
