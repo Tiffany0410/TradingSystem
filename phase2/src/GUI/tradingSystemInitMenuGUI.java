b5134bf5d3de2a647c02b210e49b07f535b7c154
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-07-28 11:06:42 +0800

    Update for trading system inital menu GUI

--- phase2/src/GUI/tradingSystemInitMenuGUI.java
+++ phase2/src/GUI/tradingSystemInitMenuGUI.java
@@ -15,19 +15,54 @@ public class tradingSystemInitMenuGUI {
     private JTextArea welcomeToTradingSystemTextArea;
     private TradingSystem tradingSystem;
 
-    public tradingSystemInitMenuGUI(){}
+    public tradingSystemInitMenuGUI(TradingSystem tradingSystem){
 
-    public void tradingSystemInitMenu(TradingSystem tradingSystem) {
         a1LoginButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              *
-             * @param e
+             * @param e click the button
              */
             @Override
             public void actionPerformed(ActionEvent e) {
                 tradingSystem.tradingSystemInital(1);
             }
         });
+
+        a2LoginAsGuestButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e click the button
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tradingSystem.tradingSystemInital(2);
+            }
+        });
+        a3CreateAccountButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e click the button
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tradingSystem.tradingSystemInital(3);
+            }
+        });
+        exitButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e click the button
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tradingSystem.tradingSystemInital(0);
+            }
+        });
+
     }
+
 }
