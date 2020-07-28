617920d1b90c55ad0c7a7fbbd4951890ea8a5905
Yu Xin Yan <eunice.yan@mail.utoronto.ca>
2020-07-28 11:01:51 -0400

    ...forgot to commit this yesterday

---
+++ phase2/src/GUI/regularUserAccountMenuGUI.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import javax.swing.*;
+
+public class regularUserAccountMenuGUI {
+    private JPanel panel1;
+    private JButton browseAllTradableItemsButton;
+    private JButton seeMostRecentThreeButton;
+    private JButton requestThatAnItemButton;
+    private JButton requestToUnfreezeAccountButton;
+    private JButton removeFromOwnInventoryButton;
+    private JButton removeFromOwnWishButton;
+    private JButton searchItemButton;
+    private JButton addToOwnWishListButton;
+    private JTextArea accountMenuIntro;
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
