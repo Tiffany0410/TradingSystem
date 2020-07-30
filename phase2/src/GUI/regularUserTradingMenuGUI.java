b01b114767def62f249ea8b21daa075e5d725bb0
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-07-30 13:42:53 +0800

    Update regularUserTradingMenuGUI

--- phase2/src/GUI/regularUserTradingMenuGUI.java
+++ phase2/src/GUI/regularUserTradingMenuGUI.java
@@ -1,157 +1,25 @@
 package GUI;
 
 import javax.swing.*;
-import java.awt.event.*;
 
-public class regularUserTradingMenuGUI extends JDialog implements GUIrunable{
-    private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
-    private JTextArea hiWelcomeToTheTextArea;
-    private JButton requestATradeButton;
-    private JButton respondToTradeRequestsButton;
-    private JButton viewOpenTradesButton;
-    private JButton viewClosedTradesButton;
-    private JButton confirmThatATradeButton;
-    private JButton seeTopThreeMostButton;
-    private JButton viewTransactionsThatHaveButton;
-    private JButton suggestionForTheMostButton;
-
-    public regularUserTradingMenuGUI() {
-        setContentPane(contentPane);
-        setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
-
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
-
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
-
-        // call onCancel() when cross is clicked
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                onCancel();
-            }
-        });
-
-        // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-        requestATradeButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        respondToTradeRequestsButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        viewOpenTradesButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        viewClosedTradesButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        confirmThatATradeButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        seeTopThreeMostButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        viewTransactionsThatHaveButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        suggestionForTheMostButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                
-            }
-        });
-    }
-
-    private void onOK() {
-        // add your code here
-        dispose();
-    }
-
-    private void onCancel() {
-        // add your code here if necessary
-        dispose();
-    }
+public class regularUserTradingMenuGUI {
+    private JPanel rootPanel;
 
     public void run() {
-        regularUserTradingMenuGUI dialog = new regularUserTradingMenuGUI();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
+        JFrame frame = new JFrame("regularUserTradingMenuGUI");
+        frame.setContentPane(new regularUserTradingMenuGUI().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
+
+    private JButton requestATradeButton;
+    private JButton respondeToTheTradeButton;
+    private JButton viewOpenTradesButton;
+    private JButton viewClosedTradesButton;
+    private JButton confirmACompletedTradeButton;
+    private JButton topThreeFrequentTradeButton;
+    private JButton viewCancelledTransactionsButton;
+    private JButton resonableTradeSuggestButton;
+    private JButton backButton;
 }
