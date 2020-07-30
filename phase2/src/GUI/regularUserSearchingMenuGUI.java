c59939290b81d3d37c50279503fa18186075562a
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-07-30 13:33:13 +0800

    Update for regularUserSearchingMenuGUI

--- phase2/src/GUI/regularUserSearchingMenuGUI.java
+++ phase2/src/GUI/regularUserSearchingMenuGUI.java
@@ -1,61 +1,20 @@
 package GUI;
 
 import javax.swing.*;
-import java.awt.event.*;
 
-public class regularUserSearchingMenuGUI extends JDialog implements GUIrunable{
-    private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
-    private JTextArea hiWelcomeToTheTextArea;
-
-    public regularUserSearchingMenuGUI() {
-        setContentPane(contentPane);
-        setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
-
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
-
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
-
-        // call onCancel() when cross is clicked
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                onCancel();
-            }
-        });
-
-        // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-    }
-
-    private void onOK() {
-        // add your code here
-        dispose();
-    }
-
-    private void onCancel() {
-        // add your code here if necessary
-        dispose();
-    }
+public class regularUserSearchingMenuGUI {
+    private JPanel rootPanel;
+    private JButton searchByXXXButton;
 
     public void run() {
-        regularUserSearchingMenuGUI dialog = new regularUserSearchingMenuGUI();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
+        JFrame frame = new JFrame("regularUserSearchingMenuGUI");
+        frame.setContentPane(new regularUserSearchingMenuGUI().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
+
+    private JButton searchByXXXButton1;
+    private JButton searchByXXXButton2;
+    private JButton backButton;
 }
