730b9b25c2ecd46619f4e271004f7e7000dd90a0
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-07-30 11:53:25 +0800

    Update regularUserAccountMenuGUI

--- phase2/src/GUI/regularUserAccountMenuGUI.java
+++ phase2/src/GUI/regularUserAccountMenuGUI.java
@@ -1,234 +1,72 @@
 package GUI;
 
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class regularUserAccountMenuGUI extends JDialog implements GUIrunable{
-    private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
-    private JTextArea hiWelcomeToTheTextArea;
-    private JButton browseAllTradableItemsButton;
-    private JButton addToOwnWishButton;
-    private JButton searchItemButton;
-    private JButton removeFromOwnWishButton;
-    private JButton removeFromOwnInventoryButton;
-    private JButton requestToUnfreezeAccountButton;
-    private JButton requestThatAnItemButton;
-    private JButton seeMostRecentThreeButton;
-    private JButton viewYourWishlistAndButton;
-    private JButton setYourOnVacationButton;
-    private JButton changeTradableStatusForButton;
-    private JButton seeUsersInYourButton;
-    private JButton changeYourHomeCityButton;
-    private JButton getSuggestionsForItemButton;
+public class regularUserAccountMenuGUI {
+    private JPanel rootPanel;
+    private JButton feedBackButton;
+    private JButton manageItemButton;
+    private JButton accountSettingButton;
+    private JButton backButton;
 
     public regularUserAccountMenuGUI() {
-
-        this.setContentPane(contentPane);
-        this.setModal(true);
-        this.getRootPane().setDefaultButton(buttonOK);
-
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
-
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
-
-        // call onCancel() when cross is clicked
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                onCancel();
-            }
-        });
-
-        // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-
-        browseAllTradableItemsButton.addActionListener(new ActionListener() {
+        feedBackButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              *
-             * @param e the event to be processed
+             * @param e
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                //TODO: Call regular user feed back menu and close this window
             }
         });
-
-
-        addToOwnWishButton.addActionListener(new ActionListener() {
+        manageItemButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              *
-             * @param e the event to be processed
+             * @param e
              */
             @Override
             public void actionPerformed(ActionEvent e) {
+                //TODO: Call manage Item menu and close this window
 
             }
         });
-        searchItemButton.addActionListener(new ActionListener() {
+        accountSettingButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              *
-             * @param e the event to be processed
+             * @param e
              */
             @Override
             public void actionPerformed(ActionEvent e) {
+                //TODO; Call account setting menu and close this window
 
             }
         });
-        removeFromOwnWishButton.addActionListener(new ActionListener() {
+        backButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
              *
-             * @param e the event to be processed
+             * @param e
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                //TODO: Go back to regular user main menu and close this window
             }
         });
-        removeFromOwnInventoryButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        requestToUnfreezeAccountButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        requestThatAnItemButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        seeMostRecentThreeButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        viewYourWishlistAndButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        setYourOnVacationButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        changeTradableStatusForButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        seeUsersInYourButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        changeYourHomeCityButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        getSuggestionsForItemButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-    }
-
-    private void onOK() {
-        // add your code here
-        dispose();
-    }
-
-    private void onCancel() {
-        // add your code here if necessary
-        dispose();
     }
 
     public void run() {
-        regularUserAccountMenuGUI dialog = new regularUserAccountMenuGUI();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
+        JFrame frame = new JFrame("regularUserAccountMenuGUI");
+        frame.setContentPane(new regularUserAccountMenuGUI().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
+
 
 }
