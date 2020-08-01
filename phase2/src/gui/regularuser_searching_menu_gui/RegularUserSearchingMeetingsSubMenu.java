0cd104a96067bd3f7ecf0abfd944cfe0ae9dc3c5
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-08-01 21:22:50 +0800

    Update RegularUserSearchingMeetingsSubMenu

--- phase2/src/gui/regularuser_searching_menu_gui/RegularUserSearchingMeetingsSubMenu.java
+++ phase2/src/gui/regularuser_searching_menu_gui/RegularUserSearchingMeetingsSubMenu.java
@@ -1,4 +1,78 @@
 package gui.regularuser_searching_menu_gui;
 
+import controllers.regularusersubcontrollers.RegularUserSearchingMenuController;
+import gui.GUIDemo;
+import gui.GUIUserInputInfo;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class RegularUserSearchingMeetingsSubMenu {
+    private JPanel rootPanel;
+    private JButton sortByDateButton;
+    private JButton incompleteMeetingButton;
+    private JButton completeMeetingButton;
+    private JButton backButton;
+
+    public RegularUserSearchingMeetingsSubMenu(RegularUserSearchingMenuController regularUserSearchingMenuController, GUIDemo guiDemo) {
+        sortByDateButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                regularUserSearchingMenuController.allMeetingSortByDate();
+                // TODO: Need method to close this window
+            }
+        });
+        incompleteMeetingButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                regularUserSearchingMenuController.unCompleteMeetingSortByDate();
+                // TODO: Need method to close this window
+            }
+        });
+        completeMeetingButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                regularUserSearchingMenuController.completeMeetingSortByDate();
+                // TODO: Need method to close this window
+            }
+        });
+        backButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guiDemo.runRegularUserSearchingMenuGUI();
+                // TODO: Need method to close this window
+            }
+        });
+    }
+
+    public void run(RegularUserSearchingMenuController regularUserSearchingMenuController, GUIDemo guiDemo) {
+        JFrame frame = new JFrame("RegularUserSearchingMeetingsSubMenu");
+        frame.setContentPane(new RegularUserSearchingMeetingsSubMenu(regularUserSearchingMenuController, guiDemo).rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
 }
