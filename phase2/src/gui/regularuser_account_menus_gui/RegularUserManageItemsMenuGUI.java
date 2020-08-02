000640786ef16fff64e2ff13b06b2896d68a1d69
shitang <tiffanyshi.tang@mail.utoronto.ca>
2020-08-02 19:34:11 -0400

    Update RegularUserManageItemsMenuGUI

--- phase2/src/gui/regularuser_account_menus_gui/RegularUserManageItemsMenuGUI.java
+++ phase2/src/gui/regularuser_account_menus_gui/RegularUserManageItemsMenuGUI.java
@@ -1,4 +1,99 @@
 package gui.regularuser_account_menus_gui;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class RegularUserManageItemsMenuGUI {
+    private JPanel rootPanel;
+    private JButton browseAllTradableButton;
+    private JButton addToWishListButton;
+    private JButton removeFromWishListButton;
+    private JButton removeFromInventoryButton;
+    private JButton requestItemButton;
+    private JButton mostRecentThreeItemsTradedButton;
+    private JButton viewWishListInventoryButton;
+    private JButton changeTradableStatusForItemButton;
+    private JButton getSuggestionForItemToLendButton;
+    private JButton backButton;
+
+    public RegularUserManageItemsMenuGUI(){
+        browseAllTradableButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Browse all tradable items in the system
+            }
+        });
+
+        addToWishListButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Add to own Wish List
+            }
+        });
+
+        removeFromWishListButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Remove from Wish List
+            }
+        });
+
+        removeFromInventoryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Remove from own Inventory
+            }
+        });
+
+        requestItemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Request that an item be added to your inventory
+            }
+        });
+
+        mostRecentThreeItemsTradedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: See most recent three items traded
+            }
+        });
+
+        viewWishListInventoryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: View your wishlist and inventory
+            }
+        });
+
+        changeTradableStatusForItemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Change tradable status for an inventory item
+            }
+        });
+
+        getSuggestionForItemToLendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Get suggestions for item(s) that you can lend to a given user
+            }
+        });
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Back to RegularUserAccountMainMenu
+            }
+        });
+    }
+
+    public void run() {
+        JFrame frame = new JFrame("RegularUserManageItemsMenuGUI");
+        frame.setContentPane(new RegularUserManageItemsMenuGUI().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
