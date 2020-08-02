c7b2de35f74bc08a82988a4c411e32530b38f7c8
shitang <tiffanyshi.tang@mail.utoronto.ca>
2020-08-02 19:47:40 -0400

    Updated RegularUserAccountSettingsMenuGUI and RegularUserFollowMenuGUI forms

--- phase2/src/gui/regularuser_account_menus_gui/RegularUserAccountSettingsMenuGUI.java
+++ phase2/src/gui/regularuser_account_menus_gui/RegularUserAccountSettingsMenuGUI.java
@@ -1,4 +1,10 @@
 package gui.regularuser_account_menus_gui;
 
+import javax.swing.*;
+
 public class RegularUserAccountSettingsMenuGUI {
+    private JButton requestToUnfreezeAccountButton;
+    private JButton setYourOnVacationButton;
+    private JButton changeYourHomeCityButton;
+    private JButton backButton;
 }
--- phase2/src/gui/regularuser_account_menus_gui/RegularUserFollowMenuGUI.java
+++ phase2/src/gui/regularuser_account_menus_gui/RegularUserFollowMenuGUI.java
@@ -1,4 +1,11 @@
 package gui.regularuser_account_menus_gui;
 
+import javax.swing.*;
+
 public class RegularUserFollowMenuGUI {
+    private JButton followAnUserButton;
+    private JButton followAnItemButton;
+    private JButton seeRecentStatusOfButton;
+    private JButton seeRecentStatusOfButton1;
+    private JButton backButton;
 }
