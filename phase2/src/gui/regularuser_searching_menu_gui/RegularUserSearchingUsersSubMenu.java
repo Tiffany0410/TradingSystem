1fb08ac43bc6aa8229e08b41aef8b1dfabed1f59
gongjiaqi <jiaqi.gong@mail.utoronto.ca>
2020-08-05 06:00:53 +0800

    Update RegularUserSearchingUsersSubMenu

--- phase2/src/gui/regularuser_searching_menu_gui/RegularUserSearchingUsersSubMenu.java
+++ phase2/src/gui/regularuser_searching_menu_gui/RegularUserSearchingUsersSubMenu.java
@@ -1,11 +1,17 @@
 package gui.regularuser_searching_menu_gui;
 
 import controllers.regularusersubcontrollers.RegularUserSearchingMenuController;
+import exception.InvalidIdException;
 import gui.GUIDemo;
+import managers.usermanager.TradableUser;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import presenter.SystemMessage;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegularUserSearchingUsersSubMenu {
     private JPanel rootPanel;
@@ -14,7 +20,8 @@ public class RegularUserSearchingUsersSubMenu {
     private JButton sortUserByRatingButton;
     private JButton backButton;
 
-    public RegularUserSearchingUsersSubMenu(RegularUserSearchingMenuController regularUserSearchingMenuController, GUIDemo guiDemo) {
+    public RegularUserSearchingUsersSubMenu(RegularUserSearchingMenuController regularUserSearchingMenuController,
+                                            GUIDemo guiDemo, SystemMessage systemMessage) {
         recentTradeUserButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
@@ -23,10 +30,18 @@ public class RegularUserSearchingUsersSubMenu {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                regularUserSearchingMenuController.recentThreePartner();
-                // TODO: Need method to close this window
 
-            }
+                try{
+                    List<Integer> filter = regularUserSearchingMenuController.recentThreePartner();
+                if (filter.size() == 0) {
+                    systemMessage.msgForNothing();
+                } else {
+                    systemMessage.printResult(new ArrayList<>(filter)); }
+                } catch ( InvalidIdException ex) {
+                    systemMessage.printInvalidID(); }
+                    // TODO: Need method to close this window
+
+                }
         });
         frequentTradeUserButton.addActionListener(new ActionListener() {
             /**
@@ -36,7 +51,18 @@ public class RegularUserSearchingUsersSubMenu {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                regularUserSearchingMenuController.sortAllTradedPartner();
+
+                try {
+                    List<Integer> filter = regularUserSearchingMenuController.sortAllTradedPartner();
+                    if (filter.size() == 0) {
+                        systemMessage.msgForNothing();
+                    } else {
+                        systemMessage.printResult(new ArrayList<>(filter));
+                    }
+                } catch (InvalidIdException ex) {
+                    systemMessage.printInvalidID();
+                }
+
                 // TODO: Need method to close this window
 
             }
@@ -49,7 +75,14 @@ public class RegularUserSearchingUsersSubMenu {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                regularUserSearchingMenuController.sortRating();
+
+
+                List<TradableUser> l = regularUserSearchingMenuController.sortRating();
+                if (l.size() == 0) {
+                    systemMessage.msgForNothing();
+                } else {
+                    systemMessage.printResult(new ArrayList<>(l));
+                }
                 // TODO: Need method to close this window
 
             }
@@ -69,9 +102,10 @@ public class RegularUserSearchingUsersSubMenu {
         });
     }
 
-    public void run(RegularUserSearchingMenuController regularUserSearchingMenuController, GUIDemo guiDemo) {
+    public void run(RegularUserSearchingMenuController regularUserSearchingMenuController,
+                    GUIDemo guiDemo, SystemMessage systemMessage) {
         JFrame frame = new JFrame("RegularUserSearchingUsersSubMenu");
-        frame.setContentPane(new RegularUserSearchingUsersSubMenu(regularUserSearchingMenuController, guiDemo).rootPanel);
+        frame.setContentPane(new RegularUserSearchingUsersSubMenu(regularUserSearchingMenuController, guiDemo, systemMessage).rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
