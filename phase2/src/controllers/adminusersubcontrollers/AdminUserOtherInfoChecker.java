ac84ff18c0ed785191d4f6ffaeee9ebbb0fec2b6
Yu Xin Yan <eunice.yan@mail.utoronto.ca>
2020-08-06 18:22:42 -0400

    deleted two methods in AdminUserOtherInfoChecker class

--- phase2/src/controllers/adminusersubcontrollers/AdminUserOtherInfoChecker.java
+++ phase2/src/controllers/adminusersubcontrollers/AdminUserOtherInfoChecker.java
@@ -31,69 +31,6 @@ public class AdminUserOtherInfoChecker {
         this.um = um;
     }
 
-    /**
-     * Gets the item number from the admin user.
-     * @param numItemsToAdd The list of items to be added to regular users inventories.
-     * @return The number of the item in the list.
-     */
-    public int getItem(int numItemsToAdd){
-        //TODO: delete later + replace with check
-        /*
-         * Referenced the code in the first answer in
-         * https://stackoverflow.com/questions/32592922/java-try-catch-with-scanner
-         * by answerer Yassine.b
-         */
-        // asks for the item name, description, owner id of the user to be added
-        boolean okInput = false;
-        Scanner sc = new Scanner(System.in);
-        // does not store the number of items but the number of the item the admin chooses
-        int numItem = 0;
-        do{
-            ds.printOut("Enter the number of the item in the above list (the number beside the #");
-            if(sc.hasNextInt()){
-                numItem = sc.nextInt();
-                if (1<= numItem && numItem <= numItemsToAdd) {
-                    okInput = true;
-                }
-                else{
-                    ds.printOut("Enter a proper option please");
-                }
-            }else{
-                sc.nextLine();
-                ds.printOut("Enter a valid Integer value please");
-            }
-        }while(!okInput);
-        return numItem;
-    }
-
-    /**
-     * Gets the new threshold value from the admin user.
-     * @return The new threshold value.
-     */
-    public int getThresholdAns(){
-        //TODO: delete later + replace with check
-        /*
-         * Referenced the code in the first answer in
-         * https://stackoverflow.com/questions/32592922/java-try-catch-with-scanner
-         * by answerer Yassine.b
-         */
-        boolean okInput = false;
-        Scanner sc = new Scanner(System.in);
-        int thresholdAns = 0;
-        do{
-            ds.printOut("Enter the new threshold value: ");
-            if(sc.hasNextInt()){
-                thresholdAns = sc.nextInt();
-                okInput = true;
-            }else{
-                sc.nextLine();
-                ds.printOut("Enter a valid Integer value please");
-            }
-        }while(!okInput);
-        return thresholdAns;
-    }
-
-
     public boolean checkItemToAddNum(int lenListItemToAdd, int userInput){
         return 0 <= userInput && userInput <= (lenListItemToAdd-1);
     }
