528df44320ebcf31e85b5c12bbf59d42b2fc7c56
chengleyang <chle.yang@mail.utoronto.ca>
2020-08-07 10:54:09 -0400

    temporarily block a button in RegularUserAccountMainMenuGUI

--- phase2/src/demomanager/DemoManager.java
+++ phase2/src/demomanager/DemoManager.java
@@ -48,73 +48,71 @@ public class DemoManager {
 
 
         // Start trading system
-        if (true){
-            try {
-                // Create all use classes
-                FilesReaderWriter frw = new FilesReaderWriter();
-                // Create the new UserManager or Read it from file
-                UserManager um;
-                if (frw.check_file_empty_or_not(serializedUserManagerFilePath)) { um = new UserManager();}
-                else { um = (UserManager) frw.readManagerFromFile(serializedUserManagerFilePath);}
+        try {
+            // Create all use classes
+            FilesReaderWriter frw = new FilesReaderWriter();
+            // Create the new UserManager or Read it from file
+            UserManager um;
+            if (frw.check_file_empty_or_not(serializedUserManagerFilePath)) { um = new UserManager();}
+            else { um = (UserManager) frw.readManagerFromFile(serializedUserManagerFilePath);}
 
-                // Create the new MeetingManager or Read it from file
-                MeetingManager mm;
-                if (frw.check_file_empty_or_not(serializedMeetingManagerFilePath)) { mm = new MeetingManager();}
-                else {mm = (MeetingManager) frw.readManagerFromFile(serializedMeetingManagerFilePath);}
+            // Create the new MeetingManager or Read it from file
+            MeetingManager mm;
+            if (frw.check_file_empty_or_not(serializedMeetingManagerFilePath)) { mm = new MeetingManager();}
+            else {mm = (MeetingManager) frw.readManagerFromFile(serializedMeetingManagerFilePath);}
 
-                // Create the new TradeManager or Read it from file
-                TradeManager tm;
-                if (frw.check_file_empty_or_not(serializedTradeManagerFilePath)) {tm = new TradeManager();}
-                else {tm = (TradeManager) frw.readManagerFromFile(serializedTradeManagerFilePath);}
+            // Create the new TradeManager or Read it from file
+            TradeManager tm;
+            if (frw.check_file_empty_or_not(serializedTradeManagerFilePath)) {tm = new TradeManager();}
+            else {tm = (TradeManager) frw.readManagerFromFile(serializedTradeManagerFilePath);}
 
-                // Create the new ItemManager or Read it from file
-                ItemManager im;
-                if (frw.check_file_empty_or_not(serializedItemManagerFilePath)) {im = new ItemManager();}
-                else {im = (ItemManager) frw.readManagerFromFile(serializedItemManagerFilePath);}
+            // Create the new ItemManager or Read it from file
+            ItemManager im;
+            if (frw.check_file_empty_or_not(serializedItemManagerFilePath)) {im = new ItemManager();}
+            else {im = (ItemManager) frw.readManagerFromFile(serializedItemManagerFilePath);}
 
-                // Create the new FeedbackManager or Read it from file
-                FeedbackManager fm;
-                if (frw.check_file_empty_or_not(serializedFeedbackManagerFilePath)) {fm = new FeedbackManager();}
-                else {fm = (FeedbackManager) frw.readManagerFromFile(serializedFeedbackManagerFilePath);}
+            // Create the new FeedbackManager or Read it from file
+            FeedbackManager fm;
+            if (frw.check_file_empty_or_not(serializedFeedbackManagerFilePath)) {fm = new FeedbackManager();}
+            else {fm = (FeedbackManager) frw.readManagerFromFile(serializedFeedbackManagerFilePath);}
 
-                // Create the new ActionManager or Read it from file
-                ActionManager am;
-                if (frw.check_file_empty_or_not(serializedActionManagerFilePath)) {am = new ActionManager();}
-                else {am = (ActionManager) frw.readManagerFromFile(serializedActionManagerFilePath);}
+            // Create the new ActionManager or Read it from file
+            ActionManager am;
+            if (frw.check_file_empty_or_not(serializedActionManagerFilePath)) {am = new ActionManager();}
+            else {am = (ActionManager) frw.readManagerFromFile(serializedActionManagerFilePath);}
 
-                // Create the new MessageManager or Read it from file
-                MessageManager messageManager;
-                if (frw.check_file_empty_or_not(serializedActionManagerFilePath)) {messageManager = new MessageManager();}
-                else {messageManager = (MessageManager) frw.readManagerFromFile(serializedMessageManagerFilePath);}
-
-
-                //Create the new AccountCreator
-                AccountCreator ac = new AccountCreator(um);
-
-                // Load accounts data from CSV file to initial login validator
-                Map<String, String> userLoginInfo = frw.readUserInfoFromCSVFile(userAccountInfoFilePath);
-                Map<String, String> adminUserLoginInfo = frw.readUserInfoFromCSVFile(adminAccountInfoFilePath);
-                LoginValidator lv = new LoginValidator(userLoginInfo, adminUserLoginInfo);
-                String partsOfUserAlerts = frw.readFromMenu(regUserAlertFilePath);
-                String partsOfAdminAlerts = frw.readFromMenu(adminUserAlertFilepath);
-                List<Integer> thresholdValues = frw.readThresholdValuesFromCSVFile(thresholdValuesFilePath);
+            // Create the new MessageManager or Read it from file
+            MessageManager messageManager;
+            if (frw.check_file_empty_or_not(serializedActionManagerFilePath)) {messageManager = new MessageManager();}
+            else {messageManager = (MessageManager) frw.readManagerFromFile(serializedMessageManagerFilePath);}
 
 
-                // Run GUIDemo
-                GUIDemo guiController = new GUIDemo(um, mm, tm, im, fm, messageManager,ac, lv, am, partsOfUserAlerts, partsOfAdminAlerts,
-                        (ArrayList<Integer>) thresholdValues);
-                guiController.runTradingSystemInitMenuGUI();
+            //Create the new AccountCreator
+            AccountCreator ac = new AccountCreator(um);
 
-            } catch (FileNotFoundException ex) {
-                System.out.println("Can not find file, Please check the root of the program and README file.");
+            // Load accounts data from CSV file to initial login validator
+            Map<String, String> userLoginInfo = frw.readUserInfoFromCSVFile(userAccountInfoFilePath);
+            Map<String, String> adminUserLoginInfo = frw.readUserInfoFromCSVFile(adminAccountInfoFilePath);
+            LoginValidator lv = new LoginValidator(userLoginInfo, adminUserLoginInfo);
+            String partsOfUserAlerts = frw.readFromMenu(regUserAlertFilePath);
+            String partsOfAdminAlerts = frw.readFromMenu(adminUserAlertFilepath);
+            List<Integer> thresholdValues = frw.readThresholdValuesFromCSVFile(thresholdValuesFilePath);
 
-            }catch (ClassNotFoundException ex){
-                System.out.println("Can not find Manager in related file, Please check README file or rerun the program.");
 
-            } catch (IOException ex){
-                System.out.println("Please try to clean the content of the files in Managers folder");
+            // Run GUIDemo
+            GUIDemo guiController = new GUIDemo(um, mm, tm, im, fm, messageManager,ac, lv, am, partsOfUserAlerts, partsOfAdminAlerts,
+                    (ArrayList<Integer>) thresholdValues);
+            guiController.runTradingSystemInitMenuGUI();
 
-            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Can not find file, Please check the root of the program and README file.");
+
+        }catch (ClassNotFoundException ex){
+            System.out.println("Can not find Manager in related file, Please check README file or rerun the program.");
+
+        } catch (IOException ex){
+            System.out.println("Please try to clean the content of the files in Managers folder");
+
         }
 
     }
