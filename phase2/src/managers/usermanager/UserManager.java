e6839257f45d2191695aff9ab470fa6231367874
gjchow <gjchow@users.noreply.github.com>
2020-08-09 19:32:49 -0400

    Updated UserManagerTest2

--- phase2/src/managers/usermanager/UserManager.java
+++ phase2/src/managers/usermanager/UserManager.java
@@ -2,9 +2,6 @@ package managers.usermanager;
 
 import managers.feedbackmanager.FeedbackManager;
 import managers.itemmanager.Item;
-
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
