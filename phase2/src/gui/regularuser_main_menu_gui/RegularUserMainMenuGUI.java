88cb4795ebd4ef5da081e5792affedcbc3a06e81
Yu Xin Yan <eunice.yan@mail.utoronto.ca>
2020-08-08 22:26:10 -0400

    now guests can view all the options but can go in the browse all tradable items option

--- phase2/src/gui/regularuser_main_menu_gui/RegularUserMainMenuGUI.java
+++ phase2/src/gui/regularuser_main_menu_gui/RegularUserMainMenuGUI.java
@@ -77,10 +77,7 @@ public class RegularUserMainMenuGUI {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (guest){
-                    guiD.printNotification(sm.msgForGuest());
-                }
-                else if (amc.seeIfFrozen()){
+                if (amc.seeIfFrozen()){
                     guiD.printNotification(sm.lockMessageForFrozen());
                 }
                 else if (amc.seeIfOnVacation()){
@@ -100,10 +97,7 @@ public class RegularUserMainMenuGUI {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (guest){
-                    guiD.printNotification(sm.msgForGuest());
-                }
-                else if (amc.seeIfFrozen()){
+                if (amc.seeIfFrozen()){
                     guiD.printNotification(sm.lockMessageForFrozen());
                 }
                 else if (amc.seeIfOnVacation()){
@@ -125,10 +119,7 @@ public class RegularUserMainMenuGUI {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (guest){
-                    guiD.printNotification(sm.msgForGuest());
-                }
-                else if (amc.seeIfFrozen()){
+                if (amc.seeIfFrozen()){
                     guiD.printNotification(sm.lockMessageForFrozen());
                 }
                 else{
